66da3249174b6aa4b7d13bf30ca65cbc71670e02
NioZhao <NioZhao@viatech.com.cn>
2020-11-30 10:39:55 +0800

    [SocketBuf] fix issue about socket receive buffer size is too small in some case.

--- modules/access/live555.cpp
+++ modules/access/live555.cpp
@@ -2,7 +2,7 @@
  * live555.cpp : LIVE555 Streaming Media support.
  *****************************************************************************
  * Copyright (C) 2003-2007 VLC authors and VideoLAN
- * $Id$
+ * $Id: 141556c17d1ff8690b4ceacee72b649f8be1c2bd $
  *
  * Authors: Laurent Aimar <fenrir@via.ecp.fr>
  *          Derk-Jan Hartman <hartman at videolan. org>
@@ -1941,8 +1941,53 @@ static void StreamRead( void *p_private, unsigned int i_size,
 
     //msg_Dbg( p_demux, "pts: %d", pts.tv_sec );
 
-    int64_t i_pts = (int64_t)pts.tv_sec * INT64_C(1000000) +
-        (int64_t)pts.tv_usec;
+    int64_t i_pts = (int64_t)pts.tv_sec * INT64_C(1000000) + (int64_t)pts.tv_usec;
+    
+	static int64_t begin_time = i_pts;
+	if (i_pts - begin_time >= 5 * INT64_C(1000000))
+	{
+		begin_time = i_pts;
+		//每隔一段时间检查下socket receive buffer大小，因为播放组播rtsp时，若本机就是组播服务器则会出现buffer大小很小0或者2K，
+		//导致接收数据不完整，从而出现花屏现象。
+		MediaSubsession         *sub = NULL;
+		MediaSubsessionIterator iter(*p_sys->ms);
+		while ((sub = iter.next()) != NULL)
+		{
+			unsigned int   i_receive_buffer = 0;
+			bool bGet = false;
+			/* Value taken from mplayer */
+			if (!strcmp(sub->mediumName(), "audio"))
+			{
+				bGet = true;
+				i_receive_buffer = 100000;
+			}
+			else if (!strcmp(sub->mediumName(), "video"))
+			{
+				bGet = true;
+				i_receive_buffer = 2000000;
+			}
+			else
+				continue;
+
+			if (bGet && sub->rtpSource())
+			{
+				int fd = sub->rtpSource()->RTPgs()->socketNum();
+				unsigned result = 0;
+				result = getReceiveBufferSize(*p_sys->env, fd);
+
+				//msg_Info(p_demux, "streamread getReceiveBufferSize %s result:%u ", sub->mediumName(), result);
+
+				if (result < 50 * 1024)
+				{
+					/* Increase the buffer size */
+					unsigned res = 0;
+					res = increaseReceiveBufferTo(*p_sys->env, fd, i_receive_buffer);
+					//msg_Info(p_demux, "streamread increaseReceiveBufferTo %u, result:%u, info:%s", i_receive_buffer, res, buffer);
+				}
+			}
+		}
+	}
+
 
     /* XXX Beurk beurk beurk Avoid having negative value XXX */
     i_pts &= INT64_C(0x00ffffffffffffff);
@@ -2160,6 +2205,7 @@ static void StreamRead( void *p_private, unsigned int i_size,
 
     if( p_block )
     {
+
         switch( tk->format )
         {
             case live_track_t::ASF_STREAM:
